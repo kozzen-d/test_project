dca03a281a221d4f2044fbf467a0ee4b33e0ab13
kozzen-d <Akozmenkova@ukr.net>
2026-02-05 12:16:43 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello WORLD!");
         int i = 5;
-        System.out.println(i);
+        System.out.println("Test version");
     }
 }
